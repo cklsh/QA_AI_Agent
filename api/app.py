7feb8ec11a1ec7faef997f9fb97bc34c749221fb
SitiUlfah <caca@Cacas-MacBook-Pro.local>
2026-05-24 23:18:57 +0700

    add endpoint receive pdf file

--- api/app.py
+++ api/app.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 from pipeline import run_pipeline
 from rag.knowledge import init_knowledge, retrieve_context
 from llm.client import call_llm
 from models.schema import GenerateRequest, AskRequest
+import shutil
+import os
 
+from utils.document_loader import load_pdf
 app = FastAPI()
 
 # Load RAG memory once
@@ -29,6 +32,47 @@ def generate(request: GenerateRequest):
         }
 
     except Exception as e:
+        return {
+            "success": False,
+            "error": str(e)
+        }
+    
+@app.post("/generate-pdf")
+async def generate_pdf(
+    file: UploadFile = File(...)
+):
+
+    try:
+
+        # Ensure uploads folder exists
+        os.makedirs("uploads", exist_ok=True)
+
+        # Save uploaded file
+        file_path = f"uploads/{file.filename}"
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        print(f"✅ Uploaded: {file.filename}")
+
+        # Extract PDF text
+        prd_text = load_pdf(file_path)
+
+        print("\n=== EXTRACTED TEXT ===")
+        print(prd_text[:1000])
+
+        # Run pipeline
+        result = run_pipeline(prd_text)
+
+        return {
+            "success": True,
+            "filename": file.filename,
+            "extracted_length": len(prd_text),
+            "data": result
+        }
+
+    except Exception as e:
+
         return {
             "success": False,
             "error": str(e)
